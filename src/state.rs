98aa3fd2b1dc3f5ced28c457d6ed1023a062a45e
Kostyan <unordered_set@tutanota.com>
2022-12-05 00:54:50 +0000

    new state

--- src/state.rs
+++ src/state.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::{Item, Map, SnapshotMap, Strategy};
+use cw_storage_plus::{Item, VecDeque};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
@@ -13,7 +13,10 @@ pub struct State {
 pub const STATE: Item<State> = Item::new("state");
 
 #[cw_serde]
-pub struct Config {
+pub struct StakingInfo {
     pub token: Addr,
-    pub tokens_per_day: Uint128
+    pub rewards_per_day: Uint128
 }
+
+pub const DAILY_TOKEN_AMOUNT: VecDeque<Uint128> = VecDeque::new("dta");
+pub const STAKING_INFO: Item<StakingInfo> = Item::new("stakingInfo");
