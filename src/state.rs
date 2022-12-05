8084115062527831fcc121dc40a2dcf8b1e7f770
Kostyan <unordered_set@tutanota.com>
2022-12-05 00:23:14 +0000

    Start working

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::{Item, Map, SnapshotMap, Strategy};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
@@ -11,3 +11,9 @@ pub struct State {
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+#[cw_serde]
+pub struct Config {
+    pub token: Addr,
+    pub tokens_per_day: Uint128
+}
